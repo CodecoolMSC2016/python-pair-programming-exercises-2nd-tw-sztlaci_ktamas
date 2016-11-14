2d76d004757e7b1e330c069159fefb090d214eb1
ThomasKenyeres <kenyerest97@gmail.com>
2016-11-14 13:37:49 +0100

    FizzBuzz completed

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,31 @@
+def create_numbers():
+    numbers = []
+    
+    for i in range(0, 100):
+        numbers.append(int(i+1))
+
+    return numbers
+
+
 def fizzbuzz(number):
-    return
+    fizz_buzz = ""
+
+    if number % 3 == 0:
+        fizz_buzz += "Fizz"
+    if number % 5 == 0:
+        fizz_buzz += "Buzz"
+    if not fizz_buzz:
+        fizz_buzz = number
+    return fizz_buzz
 
 
 def main():
-    return
+    nums = create_numbers()
+    
+
+    for num in nums:
+        n = fizzbuzz(num)
+        print(n)
 
 if __name__ == '__main__':
     main()
